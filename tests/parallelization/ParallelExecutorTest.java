654be62cfbde338a7572170cead1b29f5bea21a5
nvpanov <crtgl@mail.ru>
2011-12-03 23:43:36 +0600

    More tests added

--- tests/parallelization/ParallelExecutorTest.java
+++ tests/parallelization/ParallelExecutorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import net.sourceforge.interval.ia_math.RealInterval;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import core.Box;
@@ -22,10 +23,10 @@ import algorithms.BaseAlgorithm;
 public class ParallelExecutorTest {
 	private Algorithm /*ParallelExecutor*/ executor;
 
-	
+	@Ignore
 	@Test(timeout=12*1000) //12 sec
-	public void test0() throws Exception {
-		fail("FIX me");
+	public void testDeJong() throws Exception {
+		//fail("FIX me for IAMath2JInterval");
 		int threads = 2* 3;
 		BaseAlgorithm algorithms[] = new BaseAlgorithm[threads];
 		for (int i = 0; i < threads; i+=2) {
@@ -33,9 +34,8 @@ public class ParallelExecutorTest {
 			algorithms[i+1] = new Bisection_UnsL_RndC_BigEqS();
 		}
 		
-		int dim = 2;
-		Function f = new Function_DeJong_nD(dim);
-		dim = 2; f = new Function_SixHumpCamelBack_2D();
+		int dim;
+		dim = 6; Function f = new Function_DeJong_nD(dim);
 		Box area = new Box(dim, new RealInterval(-10, 100));
 		
 		executor = new ParallelExecutor(threads, algorithms);
@@ -48,12 +48,32 @@ public class ParallelExecutorTest {
 		System.out.println(" >  AREA CONTAINS " + optA.length + " boxes");
 		System.out.println(" >    " + optA[0]);
 		assertTrue(opt.wid() < executor.getPrecision());
-		assertTrue(Math.abs(opt.hi() - (-1.03163)) < executor.getPrecision());
-		
+		assertTrue(Math.abs(opt.hi() - 0) < executor.getPrecision());
 	}
 	
 	@Test
-	public void test_point() {
+	public void testDeJong2() {
+		int threads = 2* 3;
+
+		int dim;
+		dim = 16; Function f = new Function_DeJong_nD(dim);
+		Box area = new Box(dim, new RealInterval(-100, 1000));
+		
+		executor = new ParallelExecutor(threads, new Bisection_SrtL_CBtC_BigEqS());
+		executor.setProblem(f, area);
+		executor.solve();
+		
+		RealInterval opt = executor.getOptimumValue();
+		Box[] optA = executor.getOptimumArea();
+		System.out.println(" > OPTIMUM VALUE: " + opt);
+		System.out.println(" >  AREA CONTAINS " + optA.length + " boxes");
+		System.out.println(" >    " + optA[0]);
+		assertTrue(opt.wid() < executor.getPrecision());
+		assertTrue(Math.abs(opt.hi() - 0) < executor.getPrecision());
+
+		
+		//dim = 2; f = new Function_SixHumpCamelBack_2D();
+		//assertTrue(Math.abs(opt.hi() - (-1.03163)) < executor.getPrecision());
 		
 		
 	}
