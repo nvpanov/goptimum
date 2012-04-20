0801ef214241bcb6fd4906902677f20121f98f36
nvpanov <crtgl@mail.ru>
2012-04-20 13:19:38 +0700

    Tests for RosenbrockG

--- tests/functions/Function_RosenbrockG_nDTest.java
+++ tests/functions/Function_RosenbrockG_nDTest.java
@@ -152,6 +152,76 @@ public class Function_RosenbrockG_nDTest {
     	assertTrue(seed + " actually: " + Math.abs(d1.lo() - checkVal2.lo()), 
 				Math.abs(d1.lo() - checkVal2.lo()) < 1e-4);
     	
+    }
+    @Test
+    public void testDerivatives() throws ExpressionException {
+    	String strExp = "100*((x1-x0^2)^2+(x2-x1^2)^2)+(x0-1)^2+(x1-1)^2";
+    	Expression exp = new Expression(strExp);
+    	FunctionNEW f = new Function_RosenbrockG_nD(3);
+    	assertEquals(strExp, f.toString());
+    	assertEquals(exp.toString(), f.toString());
+    	Gradient df = new Gradient(exp); 
+    	int dim = exp.getVariables().size();
+    	assertEquals(3, dim);
+    	// get 2nd derivatives
+    	Expression d1[] = new Expression[dim];
+    	// orig				2(200x0^3-200x0x1+x0-1)			-200x0^2+400x1^3+x1(202-400x2)-2
+    	String d1Str[] = {"-400*x0*(x1-x0^2)+2*(x0-1)", "((1-2*(x2-x1^2))*x1-x0^2)*200+2*(x1-1)", "200*(x2-x1^2)"}; 
+    	//	actual:		  								((1-2*(x2-x1^2))*x1-x0^2)*200+2*(x1-1) 200*(x2-x1^2)
+    	//												200x1-400x1x2-400x1^3-400x0^2+2x1-2
+    	//												x1(202-400x2).. OK, they are equal
+    	for (int i = 0; i < dim; i++) {
+    		d1[i] = df.getPartialDerivative(i);
+//    		if ((System.currentTimeMillis() & 1) == 0) // a kinda random
+    		Simplifier.simplify(d1[i]);
+    		assertEquals(d1Str[i], d1[i].toString());
+    	}
+
+    	
+    	Expression d2[] = new Expression[dim];
+    	for (int i = 0; i < dim; i++) {
+    		d2[i] = ( new Gradient(d1[i], exp.getVariables()) ).getPartialDerivative(i);
+    		Simplifier.simplify(d2[i]);
+    	}
+    	// check literals										
+
+      //String d2Str[] = {"1200*x0^2-400*x1+2", "1200*x1^2+202-400*x2", "200"}; //orig
+    	String d2Str[] = {"-400*(x1-3*x0^2)+2", "(1--4*x1^2-2*(x2-x1^2))*200+2", "200"};
+    	//                                          ^^ equal
+    	for (int i = 0; i < dim; i++) {
+    		assertEquals(d2Str[i], d2[i].toString());
+    	}
+
+    	// check values
+    	RealInterval f_d2Val[] = new RealInterval[dim];
+    	RealInterval exp_Val[] = new RealInterval[dim];
+    	final double val = 10;
+    	Box area = new Box(dim, new RealInterval(-val, +val));
+    	for (int i = 0; i < dim; i++) {
+    		f_d2Val[i] = f.calculate2Derivative(area, i);
+    		exp_Val[i] = d2[i].evaluate(area);
+    		assertTrue("Faild on side #"+i, f_d2Val[i].almostEquals(exp_Val[i]));
+    	}
+    	final double step = area.getInterval(0).wid()/100;
+    	double curPoint[] = new double[dim];
+    	Box curPointBox = new Box(dim, new RealInterval());
+    	for (int i=0; i < dim; i++) {
+    		curPoint[i] = area.getInterval(0).lo();
+    		curPointBox.setInterval(i, curPoint[i]);
+    	}
+//    	assert dim == 3;
+    	for (int i = 0; i < dim; i++) {
+    		while(curPoint[i] <= val) {
+    			double pointVal_exp = d2[i].evaluate(curPoint);
+    			RealInterval pointVal_f = f.calculate2Derivative(curPointBox, i);
+    			assertTrue("FAILS at point="+curPoint[i]+", i="+i, pointVal_f.contains(pointVal_exp)); 
+    			assertTrue("FAILS at point="+curPoint[i]+", i="+i, pointVal_f.wid() < 1e-3);
+    			
+    			assertTrue("FAILS at point="+curPoint[i]+", i="+i, f_d2Val[i].contains(pointVal_exp));
+    		}
+    		curPoint[i] += step;
+    		curPointBox.setInterval(i, curPoint[i]);
+    	}
     	
     }
     
--- tests/solvers/performance/RosenbrockG_PerfTest.java
+++ tests/solvers/performance/RosenbrockG_PerfTest.java
@@ -20,40 +20,41 @@ public class RosenbrockG_PerfTest {
 	public MethodRule benchmarkRun = new BenchmarkRule();
 	  
 	protected TestHarness test = new TestHarness();
-	protected final int runs = 1;
+	protected final int runs = 2;
 	protected final int warmup = 1;
-	int dim = 4;
+	int dim = 32;
 
-	//(w=1,i=3) 4/9/12
+	//(w=2(1, dim=32),i=2) 4/20/12 [-150, 60]
 	// dim									2		4		8		16		32	
-	// Bisection_SrtL_CBtC_AllEqS			0.12	0.6		16.9	--		--
-	// Bisection_SrtL_CBtC_BigEqS			0.08	0.3		1.52	6.53	30.1	
-	// PointIntervalBis_SrtL_CBtC_BigEqS	0.17	0.5		1.71	8.55	38.2
+	// Bisection_SrtL_CBtC_AllEqS			0.14	1.02	28		--		--
+	// Bisection_SrtL_CBtC_BigEqS			0.08	0.82	2.52	10		30.1	
+	// PointIntervalBis_SrtL_CBtC_BigEqS	0.19	0.73	2.1		11		38.2
 
-//	@Ignore
+	@Ignore
 	@BenchmarkOptions(benchmarkRounds = runs, warmupRounds = warmup)
 	@Test
-	public final void DeJongZeroPerf_BisectionAll() {
+	public final void RosenbrockPerf_BisectionAll() {
 		Algorithm a = new Bisection_SrtL_CBtC_AllEqS();
 		run(a);
 	}
 	
 	@BenchmarkOptions(benchmarkRounds = runs, warmupRounds = warmup)
 	@Test
-	public final void DeJongZeroPerf_BisectionBig() {
+	public final void RosenbrockPerf_BisectionBig() {
 		Algorithm a = new Bisection_SrtL_CBtC_BigEqS();
 		run(a);
 	}	
 	
 	@BenchmarkOptions(benchmarkRounds = runs, warmupRounds = warmup)
 	@Test
-	public final void DeJongZeroPerf_PointInterval() {
+	public final void RosenbrockPerf_PointInterval() {
 		Algorithm a = new PointIntervalBis_SrtL_CBtC_BigEqS();
 		run(a);
 	}	
 
 	private final void run(Algorithm a) {
 		RealInterval side = new RealInterval(-150, 60);
+		//RealInterval side = new RealInterval(-2.048, 2.048);
 		test.f_RosenbrockGn(a, dim, side);
 	}
 }
