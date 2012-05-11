0f8c3a57891e514422d5f7fea55452296ec3f1c1
nvpanov <crtgl@mail.ru>
2012-05-11 15:13:06 +0700

    Add AllSideSorter for nice output of area.

--- src/worklists/SideSorter.java
+++ src/worklists/SideSorter.java
@@ -6,7 +6,7 @@ import net.sourceforge.interval.ia_math.RealInterval;
 
 import core.Box;
 
-public class SideSorter implements Comparator<Box> {
+class SideSorter implements Comparator<Box> {
 	private final static AscendingLowBoundBoxComparator defaultComparator = 
 			new AscendingLowBoundBoxComparator();
 	private final int sideNumToSort;
@@ -25,5 +25,28 @@ public class SideSorter implements Comparator<Box> {
 		// the boxes are equal in interesting dimension
 		// return just some order
 		return defaultComparator.compare(b1, b2);
+	}
+}
+
+/*
+ * for more beautiful output of search area
+ */
+class AllSidesSorter implements Comparator<Box> {
+	@Override
+	public int compare(Box b1, Box b2) {
+		final int dim = b1.getDimension();
+		assert (dim == b2.getDimension());
+		for (int i = 0; i < dim; i++) {
+			RealInterval r1 = b1.getInterval(i);
+			RealInterval r2 = b2.getInterval(i);
+			if (r1.lo() != r2.lo())
+				return (int)Math.signum(r1.lo() - r2.lo());
+			if (r1.hi() != r2.hi())
+				return (int)Math.signum(r1.hi() - r2.hi());
+		}
+		RealInterval f1 = b1.getFunctionValue(), f2 = b2.getFunctionValue();
+		if (f1.lo() != f2.lo())
+			return (int)Math.signum(f1.lo() - f2.lo());
+		return (int)Math.signum(f1.hi() - f2.hi());
 	}
 }
