46255c1abec205dfb2b96be9c8f6f34994ecb96a
nvpanov <crtgl@mail.ru>
2013-06-09 23:08:02 -0700

    sin/cos support in ExpPropagatable + minor improvements / bug fixes

--- src/constraint/ExpressionPropagatable.java
+++ src/constraint/ExpressionPropagatable.java
@@ -18,7 +18,7 @@ import symboldiff.exceptions.ExpressionException;
  *
  */
 public class ExpressionPropagatable extends Expression {
-	boolean logging = true;
+	boolean logging = false;
 	private HashMap<Expression, RealInterval> cachedValues = new HashMap<>();
 	private Box area;
 	
@@ -30,29 +30,27 @@ public class ExpressionPropagatable extends Expression {
 		boolean wasPropagated;
 		int propagationCycles = 0;
 		init(area);
+		
 		do {
-			Box tmpForLogging = null;
-			if (logging) {
-				tmpForLogging = area.clone();
-			}
+			Box originalArea = area.clone();
 	 		wasPropagated = propagate(this, newValue);
 	 		if (wasPropagated) {
-	 			area.setFunctionValue(cachedValues.get(this));
 	 			if (logging) {
-	 				System.out.println(" Propagated: " + tmpForLogging + " => " + area);
-	 			}
+	 				System.out.println(" Propagated: " + originalArea.toStringArea() + " => " + area.toStringArea());
+	 			}	 			
 	 		}
 		} while (wasPropagated && ++propagationCycles < 100);
  		clearResources();
 		if (propagationCycles == 100)
 			throw new ArithmeticException();
+		
 		return propagationCycles > 0;
  	}
 	
 	private void init(Box area) {
 		this.area = area;
  		evaluateCaching(this, area, cachedValues);
-	}
+ 	}
 	private void clearResources() {
 		cachedValues.clear();
 		area = null;
@@ -81,35 +79,39 @@ public class ExpressionPropagatable extends Expression {
 	
 	private boolean propagate(Expression exp, RealInterval newValue) throws RepugnantConditionException 
  	{
+		assert (newValue != null);
  		if (exp.isConstant()) {
  			return false;
  		}
  		
  		RealInterval calculatedAndSavedValue = cachedValues.get(exp);
- 		if (calculatedAndSavedValue.equals(newValue)) {
+ 		if (calculatedAndSavedValue.equals(newValue) || newValue.contains(calculatedAndSavedValue)) {
+ 			// ^^ new = old || new > old
  			return false; // nothing to do, nothing was done 
  		}
  		RealInterval reducedValue = IAMath.intersect(calculatedAndSavedValue, newValue);
  		if (reducedValue == null) {
  			throw new RepugnantConditionException() ; // incompatible values
  		}
+ 		
  		if (exp.isVariable()) {
  			// it was not equal, it is not null => it was reduced
  			int index = exp.getNumberOfThisVariable();
- 			area.setInterval(index, newValue);
- 			cachedValues.put(exp, newValue);
+ 			area.setInterval(index, reducedValue);
+ 			cachedValues.put(exp, reducedValue);
  			return true;
  		}
 
  		Expression lExp = exp.getLeftExpression();
  		Expression rExp = exp.getRightExpression();
  		RealInterval lValue = null, rValue = null;
-		lValue = cachedValues.get(lExp); // this is an Operation
-		if ( rExp != null ) {
-			rValue = cachedValues.get(rExp);
+		rValue = cachedValues.get(rExp); // this is an Operation, right != null
+		if ( lExp != null ) { // left == null for unary operations
+			lValue = cachedValues.get(lExp);
 		}
  		RealInterval[] aWayToPassPointersToFunction = {reducedValue, lValue, rValue};
  		boolean wasNarrowed = narrow(exp, aWayToPassPointersToFunction);
+ 		assert (aWayToPassPointersToFunction[0] != null);
  		cachedValues.put(exp, aWayToPassPointersToFunction[0]);
  		if ( lExp != null ) {
  			wasNarrowed |= propagate(lExp, aWayToPassPointersToFunction[1]);
@@ -120,7 +122,7 @@ public class ExpressionPropagatable extends Expression {
 		return wasNarrowed; 			
  	}
  	
-	private static boolean narrow(Expression exp, RealInterval[] aWayToPassPointersToFunction) {
+	private static boolean narrow(Expression exp, RealInterval[] aWayToPassPointersToFunction) throws RepugnantConditionException {
 		try {
 			if ( exp.isAdd() )
 				return IANarrow.narrowAdd(aWayToPassPointersToFunction);
@@ -134,14 +136,20 @@ public class ExpressionPropagatable extends Expression {
 				return IANarrow.narrowPow(aWayToPassPointersToFunction);
 			if (exp.isSub())
 				return IANarrow.narrowSub(aWayToPassPointersToFunction);
+			
+			if (exp.isSin())
+				return IANarrow.narrowSin(aWayToPassPointersToFunction);
+			if (exp.isCos())
+				return IANarrow.narrowCos(aWayToPassPointersToFunction);
+			
 			if (exp.isVariable())
 				return IANarrow.narrowEquals(aWayToPassPointersToFunction);
 			if (exp.isConstant())
 				return false;
 		} catch (IANarrowingFaildException e) {
-			aWayToPassPointersToFunction[0] = null;
-			return false;
+			throw new RepugnantConditionException(); // incompatible values
 		}
-		throw new RuntimeException("Unknown operation for propagate: " + exp.getOperation());			
+		System.out.println("!!! Unknown operation for propagate: '" + exp.getOperation() + "' !!!!");
+		return false;
 	}
 }
